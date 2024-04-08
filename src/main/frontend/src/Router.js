ed7c26ea2bdd76bfebc6499514c387e687c2511e
Hoooon22 <momo990305@gmail.com>
2024-04-08 21:39:10 +0900

    sync

--- src/main/frontend/src/Router.js
+++ src/main/frontend/src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
+import SignUp from "./pages/signUp";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<Main />} />
+        <Route exact path='/signUP' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/main/frontend/src/pages/signUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SignUp() {
+//    const [id, setId] = useState('');
+
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <div className="infoTextFrame">
+                <p className="userinfoText">User Information</p>
+
+            </div>
+        </div>
+    )
+}
